4c1f4aca358e89a4610702515d24fcb98b9646b7
miffels <michael.jess@gmx.de>
2013-07-02 16:28:31 +0200

    Initial commit

---
+++ AngularJS/test/tests.js
@@ -0,0 +1,11 @@
+test( "hello test", function() {
+    ok( 1 == "1", "1 should equal 1");
+});
+
+//function write() {
+//    console.log(1);
+//    setTimeout(write, 1000);
+//    console.log(0);
+//}
+//
+//setTimeout(write, 1000);
